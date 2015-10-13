4512e8aa5cc6277f51b8dede7e8c5b8d8771dff3
佐藤 有斗 <yuitosato.w@gmail.com>
2015-10-13 13:32:46 +0900

    usersテーブルにusername, team, profile, worksカラムを追加した。ストロングパラメータにも設定

---
+++ db/migrate/20151013041304_add_username_to_user.rb
@@ -0,0 +1,8 @@
+class AddUsernameToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_column :users, :team, :string
+    add_column :users, :profile, :text
+    add_column :users, :works, :string
+  end
+end
