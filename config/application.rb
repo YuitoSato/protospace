4dc0e269df38ea86297618f8e59de9b86c296a43
佐藤 有斗 <yuitosato.w@gmail.com>
2015-10-13 21:04:23 +0900

    changed config/application.rb in order not to create test files

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module Protospace
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.generators do |g|
+        g.test_framework false
+    end
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
   end
