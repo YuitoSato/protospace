f38b0f826e198c5939c04daf33960eaa87e44696
佐藤 有斗 <yuitosato.w@gmail.com>
2015-10-18 15:51:13 +0900

    Delete methods in models/proto.rb

--- app/models/proto.rb
+++ app/models/proto.rb
@@ -1,16 +1,4 @@
 class Proto < ActiveRecord::Base
   has_many :images
   accepts_nested_attributes_for :images
-
-  def main_image
-    Image.where(proto_id: id, status: 0)
-  end
-
-  def sub1_image
-    Image.where(proto_id: id, status: 1)
-  end
-
-  def sub2_image
-    Image.where(proto_id: id, status: 2)
-  end
 end
