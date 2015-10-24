ffc162fa9e307ea5151c324b1832d7470e23db24
佐藤 有斗 <yuitosato.w@gmail.com>
2015-10-24 14:20:37 +0900

    Create the like model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021064830) do
+ActiveRecord::Schema.define(version: 20151024051801) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text",       limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.integer  "proto_id",   limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.integer  "proto_id",   limit: 4
@@ -19,6 +27,13 @@ ActiveRecord::Schema.define(version: 20151021064830) do
     t.datetime "updated_at",             null: false
     t.string   "image",      limit: 255
     t.integer  "status",     limit: 4
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "proto_id",   limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   create_table "protos", force: :cascade do |t|
