4e6fd6add980606d60714018f934e689ca157cca
佐藤 有斗 <yuitosato.w@gmail.com>
2015-10-24 21:25:08 +0900

    Create likes controller and define some actions

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,15 @@
+class LikesController < ApplicationController
+  def create
+    @like = Like.create(user_id: current_user.id, proto_id: params[:proto_id])
+    @likes = Like.where(proto_id: params[:proto_id])
+    # proto = @like.proto
+    # redirect_to proto_path(proto)
+  end
+
+  def destroy
+    like = Like.find_by(user_id: current_user.id, proto_id: params[:proto_id])
+    proto = like.proto
+    like.destroy
+    redirect_to proto_path(proto)
+  end
+end
