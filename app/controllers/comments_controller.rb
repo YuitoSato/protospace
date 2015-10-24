87ca25044c82db20cab6ac382f01e8dcf492acf7
佐藤 有斗 <yuitosato.w@gmail.com>
2015-10-24 21:34:22 +0900

    Fix the create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(create_params)
+    current_user.comments.create(create_params)
     @comments = Comment.all
   end
 
   private
   def create_params
-    params.require(:comment).permit(:text, :proto_id).merge(user_id: current_user.id)
+    params.require(:comment).permit(:text, :proto_id)
   end
 end
-
