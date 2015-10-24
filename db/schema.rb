18cd56dda9910d4d403cd860df3625143d494288
佐藤 有斗 <yuitosato.w@gmail.com>
2015-10-24 12:17:23 +0900

    Create the comment model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021064830) do
+ActiveRecord::Schema.define(version: 20151023110955) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text",       limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.integer  "proto_id",   limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.integer  "proto_id",   limit: 4
