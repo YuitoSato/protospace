40a54ebdcf6528d008f241abf17c76e40fe2ba88
佐藤 有斗 <yuitosato.w@gmail.com>
2015-10-25 15:57:14 +0900

    Fix the actions in the likes controller and protos/show.html.haml

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,15 @@ class LikesController < ApplicationController
   def create
     @like = Like.create(user_id: current_user.id, proto_id: params[:proto_id])
     @likes = Like.where(proto_id: params[:proto_id])
-    # proto = @like.proto
-    # redirect_to proto_path(proto)
+    proto = @like.proto
+    redirect_to proto_path(proto)
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, proto_id: params[:proto_id])
     proto = like.proto
     like.destroy
+    @likes = Like.where(proto_id: params[:proto_id])
     redirect_to proto_path(proto)
   end
 end
