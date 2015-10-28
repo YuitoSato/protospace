d742feac5a4376164ad6189536546de4d07abe48
佐藤 有斗 <yuitosato.w@gmail.com>
2015-10-28 14:19:45 +0900

    Set the counter_cache system and add the column, likes count, to the protos table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024051801) do
+ActiveRecord::Schema.define(version: 20151028044115) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text",       limit: 65535
@@ -37,12 +37,13 @@ ActiveRecord::Schema.define(version: 20151024051801) do
   end
 
   create_table "protos", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "catch_copy", limit: 65535
-    t.text     "concept",    limit: 65535
-    t.integer  "user_id",    limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "title",       limit: 255
+    t.text     "catch_copy",  limit: 65535
+    t.text     "concept",     limit: 65535
+    t.integer  "user_id",     limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "likes_count", limit: 4
   end
 
   create_table "taggings", force: :cascade do |t|
