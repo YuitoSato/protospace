a28f6fbd2228b1b50eb7a9c079cbcaf7ce651ad9
佐藤 有斗 <yuitosato.w@gmail.com>
2015-10-31 17:37:02 +0900

    Fix javascripts/form_support.js

--- app/assets/javascripts/form_support.js
+++ app/assets/javascripts/form_support.js
@@ -1,18 +1,33 @@
-function main_image_preview(){
-  console.log("show preview!")
-  var fileList , file , fr , result, image ;
-  image = document.getElementById( "image_image") ;
-  console.log(image)
-  fileList = image.files ;
-  console.log(fileList)
-  result = document.getElementById( "main-image" ) ;
-  result.innerHTML = "" ;
-  for( var i=0 ; i<=0 ; i++){
-    file = fileList[i] ;
-    src = window.URL.createObjectURL( file ) ;
-    result.innerHTML += '<img src="' + src + '" width="auto" height="100">' ;
-  }
+$(window).on("load", function() {
+  $("#main-preview").hide();
+  $("#sub1-preview").hide();
+  $("#sub2-preview").hide();
+});
+
+$(function() {
+    $('#main-image').uploadThumbs({
+        position : '#main-preview.thumbnail',
+    });
+    $('#sub1-image').uploadThumbs({
+        position : '#sub1-preview.thumbnail',
+    });
+    $('#sub2-image').uploadThumbs({
+        position : '#sub2-preview.thumbnail',
+    });
+
+});
+
+function main_image_preview() {
+  $(".cover-image-upload").hide();
+  $("#main-preview").show();
 }
 
+function sub1_image_preview() {
+  $("#sub1-upload").hide();
+  $("#sub1-preview").show();
+}
 
-
+function sub2_image_preview() {
+  $("#sub2-upload").hide();
+  $("#sub2-preview").show();
+}
