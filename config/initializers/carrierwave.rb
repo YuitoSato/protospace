9aa1410411866c722661ec783328a56f69159b08
佐藤 有斗 <yuitosato.w@gmail.com>
2015-11-06 14:07:55 +0900

    Fix the carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,7 @@ CarrierWave.configure do |config|
   config.fog_public     = true
   config.fog_attributes = {'Cache-Control' => 'public' 'max-age=315576000'}
 
-  config.fog_directory = 's3yuito-protospace' if Rails.env.production?
+  config.fog_directory = 's3yuito-protospace'
   config.asset_host = 's3yuito-protospace'
   config.storage = :fog
 
