8709d7bcb44fecb8dd702f5c68e20b506a69f002
佐藤 有斗 <yuitosato.w@gmail.com>
2015-11-06 22:36:33 +0900

    Fix validatioon

--- app/models/proto.rb
+++ app/models/proto.rb
@@ -5,6 +5,7 @@ class Proto < ActiveRecord::Base
   has_many :comments
   accepts_nested_attributes_for :images
   acts_as_taggable_on :tags
+  validates :title, :catch_copy, presence: true
 
   def like_user(user_id)
    likes.find_by(user_id: user_id)
